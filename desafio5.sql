0e15b14290d8379e600786ad57cea0d2eede0971
Edson Pereira <ed.1206@gmail.com>
2022-10-25 22:09:14 -0300

    Criando uma QUERY que exibe os dados corretos nas colunas cancao e reproducoes

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT s.song_name AS cancao,
+COUNT(h.song_id) AS reproducoes
+FROM SpotifyClone.songs AS s
+INNER JOIN SpotifyClone.reproduction_history AS h
+ON s.song_id = h.song_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
