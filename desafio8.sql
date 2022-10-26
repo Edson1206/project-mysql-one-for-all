e207369502cab1283c3d19dd0ce2e24920cab040
Edson Pereira <ed.1206@gmail.com>
2022-10-25 22:41:56 -0300

    Criando uma QUERY que retorna a coluna artista contendo o nome da pessoa artista e a coluna album contendo o nome do álbum

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT a.artist_name AS artista,
+b.album_name AS album
+FROM SpotifyClone.artists AS a
+INNER JOIN SpotifyClone.albums AS b
+ON a.artist_id = b.artist_id
+WHERE a.artist_name = 'Elis Regina'
+ORDER BY album;
